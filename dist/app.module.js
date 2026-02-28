f3f34c1905e67ee125323d375c65f6890a5ff1b1
Ayush Rawat <rwtayush@Ayushs-MacBook-Air.local>
2026-02-28 22:00:46 +0530

    connecting the Db to project

--- dist/app.module.js
+++ dist/app.module.js
@@ -5,15 +5,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const mongoose_1 = require("@nestjs/mongoose");
+const mongoose_2 = require("mongoose");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const auth_module_1 = require("./auth/auth.module");
 const user_module_1 = require("./user/user.module");
 let AppModule = class AppModule {
+    connection;
+    logger = new common_1.Logger('Database');
+    constructor(connection) {
+        this.connection = connection;
+    }
+    onModuleInit() {
+        if (this.connection.readyState === 1) {
+            console.log("Database is connected successfully");
+        }
+        this.connection.on('error', (error) => {
+            this.logger.error('✗ Database connection error: ' + error.message);
+        });
+        this.connection.on('disconnected', () => {
+            this.logger.warn('Database disconnected');
+        });
+    }
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
@@ -25,6 +48,8 @@ exports.AppModule = AppModule = __decorate([
         ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
-    })
+    }),
+    __param(0, (0, mongoose_1.InjectConnection)()),
+    __metadata("design:paramtypes", [mongoose_2.Connection])
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module, OnModuleInit, Logger } from '@nestjs/common';
+import { MongooseModule, InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -16,4 +17,23 @@ import { UserModule } from './user/user.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements OnModuleInit {
+  private logger = new Logger('Database');
+
+  constructor(@InjectConnection() private connection: Connection) {}
+
+  onModuleInit() {
+    // Check if already connected
+    if (this.connection.readyState === 1) {
+      console.log("Database is connected successfully");
+      
+    }
+    this.connection.on('error', (error) => {
+      this.logger.error('✗ Database connection error: ' + error.message);
+    });
+
+    this.connection.on('disconnected', () => {
+      this.logger.warn('Database disconnected');
+    });
+  }
+}
