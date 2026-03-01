93dc3dbcb13aad29cfea1f318e8c21344cff7b55
Ayush Rawat <rwtayush@Ayushs-MacBook-Air.local>
2026-03-02 00:17:22 +0530

    adding DTO Validation

--- src/auth/dto/registerUser.dto.ts
+++ src/auth/dto/registerUser.dto.ts
@@ -1,6 +1,15 @@
+import {IsEmail, IsString, IsNotEmpty} from 'class-validator';
 export class RegisterDto {
+    @IsString()
+    @IsNotEmpty()
     fname: string;
+    @IsString()
+    @IsNotEmpty()
     lname: string;
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
